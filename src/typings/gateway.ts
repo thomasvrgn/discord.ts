0680fff89aeb2f42b0920d2697e43ab4b283f24e
NessMC <contact@nessmc.fr>
2020-10-07 16:02:20 +0200

    🏷️ Heartbeat interface added.

--- src/typings/gateway.ts
+++ src/typings/gateway.ts
@@ -1,11 +1,8 @@
+import { Heartbeat } from './heartbeat';
+
 export interface GatewayResponse {
   t: any,
   s: any,
   op: number,
   d: Heartbeat,
-}
-
-export interface Heartbeat {
-  heartbeat_interval: number,
-  _trace?: Array<string>,
 }
---
+++ src/typings/heartbeat.ts
@@ -0,0 +1,4 @@
+export interface Heartbeat {
+  heartbeat_interval: number,
+  _trace?: Array<string>,
+}
